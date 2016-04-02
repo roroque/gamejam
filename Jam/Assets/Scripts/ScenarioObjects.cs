708157ceeb2ef0b395ce0e657e836c8e9adc58b0
Neves <nevesferreira.gabriel@hotmail.com>
2016-04-02 17:11:04 -0300

    with lucas

---
+++ Jam/Assets/Scripts/ScenarioObjects.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenarioObjects : MonoBehaviour {
+
+	public float speed;
+	public Rigidbody2D body;
+
+	// Use this for initialization
+	void Start () {
+	
+		body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		body.velocity = new Vector2(speed * Time.deltaTime,0);
+	
+	}
+}
