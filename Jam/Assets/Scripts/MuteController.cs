6216f6006687b666015dc30129dd800cd2d198d7
Lucas Bonin <lucasbonin369@gmail.com>
2016-04-02 12:55:23 -0300

    Mute Function

---
+++ Jam/Assets/Scripts/MuteController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+//[RequireComponent(typeof (PlatformerCharacter2D))]
+
+public class MuteController : MonoBehaviour
+{
+
+	private bool isMute = false; 
+	public Sprite mute;
+	public Sprite noMute;
+
+
+	public void OnClick ()
+	{
+		isMute = !isMute;
+		if (isMute) {
+			print ("Mutou");
+
+			AudioListener.volume = 0.0f;
+			this.GetComponent<Image>().sprite = mute;
+		}
+		else{
+			print ("naoMutou");
+
+			AudioListener.volume = 1.0f;
+			this.GetComponent<Image>().sprite = noMute;
+		}
+	}
+
+//	public void Update()
+//	{
+//		if (AudioListener.volume == 0.0f) {
+//			isMute = true;
+//			this.GetComponent<Image> ().sprite = mute;
+//		}
+//		else{
+//			isMute = false;
+//
+//			this.GetComponent<Image>().sprite = noMute;
+//		}
+//	}
+}
