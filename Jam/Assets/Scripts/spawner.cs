1886b1ba930492d18b5b203ea795b545fe5ebfe9
Neves <nevesferreira.gabriel@hotmail.com>
2016-04-02 04:34:28 -0300

    spawner and spawner killer

---
+++ Jam/Assets/Scripts/spawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spawner : MonoBehaviour {
+
+	public float timer;
+	public float counter;
+	public float minTime;
+	public float maxTime;
+	public float nextTime;
+	public GameObject[] enemies;
+
+
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+		nextTime = maxTime;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		timer += Time.deltaTime;
+		if(timer >= nextTime){
+			timer = 0;
+			spawn();
+
+		}
+
+	}
+
+
+	void spawn(){
+
+		var stop = GameObject.FindGameObjectWithTag("GameController").GetComponent<MissionBehaviour>().stopCounting;
+
+		if(!stop){
+			nextTime = Random.Range(minTime,maxTime);
+			var x = Instantiate(enemies[Random.Range(0,enemies.Length -1)],transform.position,transform.rotation);
+		}
+
+
+	}
+
+}
