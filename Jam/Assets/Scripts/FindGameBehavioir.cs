5a780aad660add12b6d2c2b34452d4f88462ed70
Lucas Bonin <lucasbonin369@gmail.com>
2016-04-02 17:09:23 -0300

    Bug GameBehaviour

---
+++ Jam/Assets/Scripts/FindGameBehavioir.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FindGameBehavioir : MonoBehaviour {
+
+	private GameBehaviour behave;
+
+
+
+	public void GoToMothership(AudioSource audio){
+		GameBehaviour behave = GameObject.FindGameObjectWithTag("GameBehaviour").GetComponent<GameBehaviour>();
+
+
+		behave.GoToMothership(audio);
+
+	}
+
+
+	public void GoToLeaderbords(AudioSource audio){
+		GameBehaviour behave = GameObject.FindGameObjectWithTag("GameBehaviour").GetComponent<GameBehaviour>();
+
+
+		behave.GoToLeaderbords(audio);
+
+
+	}
+
+	public void GoToCredits(AudioSource audio){
+		GameBehaviour behave = GameObject.FindGameObjectWithTag("GameBehaviour").GetComponent<GameBehaviour>();
+
+
+		behave.GoToCredits (audio);
+
+
+	}
+
+
+	public void GoToLevel(AudioSource audio){
+		GameBehaviour behave = GameObject.FindGameObjectWithTag("GameBehaviour").GetComponent<GameBehaviour>();
+
+		behave.GoToLevel (audio);
+	}
+
+
+}
